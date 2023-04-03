020b22d1f7d3d636f2924423d20637bd507f1c37
Sonja Laurila <laurila@google.com>
2023-04-03 16:49:18 +0200

    Add TODO for getting rid of the cjs wrapper

--- index.cjs
+++ index.cjs
@@ -2,5 +2,6 @@
 // breaking change on how WebBundlePlugin is imported.
 // For context, see:
 // https://github.com/evanw/esbuild/issues/532#issuecomment-1019392638
+// TODO: Get rid of this together with the next other breaking change.
 const { WebBundlePlugin } = require('./lib/index.cjs');
 module.exports = WebBundlePlugin;
