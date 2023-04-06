8226ac7ab49552e2e5d46371462a2c28267ab70d
Christian Flach <cmfcmf@chromium.org>
2023-04-06 11:45:03 +0200

    Apply eslint to source files

--- packages/webbundle-webpack-plugin/test/fixtures/app.js
+++ packages/webbundle-webpack-plugin/test/fixtures/app.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * Copyright 2020 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,4 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 console.log('Hello, world!');
