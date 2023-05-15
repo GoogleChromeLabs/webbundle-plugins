0433ac38235fd99b383a79ceabe5e0a1924d9ac0
Sonja <46299415+sonkkeli@users.noreply.github.com>
2023-05-15 11:37:45 +0200

    Refactor PluginOptions validation logic (#54)
    Use "zod" to validate the plugin options.

--- packages/shared/utils.ts
+++ packages/shared/utils.ts
@@ -17,11 +17,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import mime from 'mime';
-import { KeyObject } from 'crypto';
 import { combineHeadersForUrl, BundleBuilder } from 'wbn';
 import { IntegrityBlockSigner, WebBundleId } from 'wbn-sign';
-import { checkAndAddIwaHeaders, maybeSetIwaDefaults } from './iwa-headers';
-import { PluginOptions } from './types';
+import { checkAndAddIwaHeaders } from './iwa-headers';
+import { ValidIbSignPluginOptions, ValidPluginOptions } from './types';
 
 // If the file name is 'index.html', create an entry for both baseURL/dir/
 // and baseURL/dir/index.html which redirects to the aforementioned. Otherwise
@@ -31,7 +30,7 @@ export function addAsset(
   baseURL: string,
   relativeAssetPath: string, // Asset's path relative to app's base dir. E.g. sub-dir/helloworld.js
   assetContentBuffer: Uint8Array | string,
-  pluginOptions: PluginOptions
+  pluginOptions: ValidPluginOptions
 ) {
   const parsedAssetPath = path.parse(relativeAssetPath);
   const isIndexHtmlFile = parsedAssetPath.base === 'index.html';
@@ -39,7 +38,7 @@ export function addAsset(
   // For object type, the IWA headers have already been check in constructor.
   const shouldCheckIwaHeaders =
     typeof pluginOptions.headerOverride === 'function' &&
-    pluginOptions.integrityBlockSign &&
+    'integrityBlockSign' in pluginOptions &&
     pluginOptions.integrityBlockSign.isIwa;
 
   if (isIndexHtmlFile) {
@@ -82,12 +81,9 @@ export function addFilesRecursively(
   builder: BundleBuilder,
   baseURL: string,
   dir: string,
-  pluginOptions: PluginOptions,
+  pluginOptions: ValidPluginOptions,
   recPath = ''
 ) {
-  if (baseURL !== '' && !baseURL.endsWith('/')) {
-    throw new Error("Non-empty baseURL must end with '/'.");
-  }
   const files = fs.readdirSync(dir);
   files.sort(); // Sort entries for reproducibility.
 
@@ -117,58 +113,19 @@ export function addFilesRecursively(
   }
 }
 
-function validateIntegrityBlockOptions(opts: PluginOptions) {
-  if (!opts.integrityBlockSign) {
-    return;
-  }
-
-  maybeSetIwaDefaults(opts);
-
-  if (opts.primaryURL !== undefined) {
-    throw new Error('Primary URL is not supported for Isolated Web Apps.');
-  }
-
-  if (opts.baseURL !== '') {
-    const expectedOrigin = new WebBundleId(
-      opts.integrityBlockSign.key
-    ).serializeWithIsolatedWebAppOrigin();
-
-    if (opts.baseURL !== expectedOrigin) {
-      throw new Error(
-        `The provided "baseURL" option (${opts.baseURL}) does not match the expected base URL (${expectedOrigin}), which is derived from the provided private key`
-      );
-    }
-  }
-
-  if (
-    opts.integrityBlockSign.isIwa === true &&
-    typeof opts.headerOverride === 'object'
-  ) {
-    checkAndAddIwaHeaders(opts.headerOverride);
-  }
-}
-
-export function validateOptions(opts: PluginOptions) {
-  if (opts.baseURL !== '' && !opts.baseURL.endsWith('/')) {
-    throw new Error('Non-empty baseURL must end with "/".');
-  }
-  validateIntegrityBlockOptions(opts);
-}
-
-export function maybeSignWebBundle(
+export function getSignedWebBundle(
   webBundle: Uint8Array,
-  opts: PluginOptions,
-  loggingCallback: (key: KeyObject) => void
+  opts: ValidIbSignPluginOptions,
+  infoLogger: (str: string) => void
 ): Uint8Array {
-  if (!opts.integrityBlockSign) {
-    return webBundle;
-  }
-
   const { signedWebBundle } = new IntegrityBlockSigner(webBundle, {
     key: opts.integrityBlockSign.key,
   }).sign();
 
-  loggingCallback(opts.integrityBlockSign.key);
+  const origin = new WebBundleId(
+    opts.integrityBlockSign.key
+  ).serializeWithIsolatedWebAppOrigin();
 
+  infoLogger(origin);
   return signedWebBundle;
 }
--- packages/webbundle-webpack-plugin/src/index.ts
+++ packages/webbundle-webpack-plugin/src/index.ts
@@ -15,24 +15,19 @@
  */
 
 import webpack, { Compiler, Compilation, WebpackPluginInstance } from 'webpack';
-import { KeyObject } from 'crypto';
 import { BundleBuilder } from 'wbn';
-import { WebBundleId } from 'wbn-sign';
 import {
   addAsset,
   addFilesRecursively,
-  validateOptions,
-  maybeSignWebBundle,
+  getSignedWebBundle,
 } from '../../shared/utils';
-import { PluginOptions } from '../../shared/types';
+import {
+  getValidatedOptionsWithDefaults,
+  PluginOptions,
+  ValidPluginOptions,
+} from '../../shared/types';
 
 const PLUGIN_NAME = 'webbundle-webpack-plugin';
-
-const defaults = {
-  formatVersion: 'b2',
-  output: 'out.wbn',
-  baseURL: '',
-};
 
 // Returns if the semantic version number of Webpack is 4.
 function isWebpackMajorV4(): boolean {
@@ -40,23 +35,22 @@ function isWebpackMajorV4(): boolean {
 }
 
 export class WebBundlePlugin implements WebpackPluginInstance {
-  private opts: PluginOptions;
+  private validOpts: ValidPluginOptions;
 
-  constructor(opts: PluginOptions) {
-    this.opts = Object.assign({}, defaults, opts);
-    validateOptions(this.opts);
+  constructor(rawOpts: PluginOptions) {
+    this.validOpts = getValidatedOptionsWithDefaults(rawOpts);
   }
 
   process = (compilation: Compilation) => {
-    const opts = this.opts;
+    const opts = this.validOpts;
     const builder = new BundleBuilder(opts.formatVersion);
-    if (opts.primaryURL) {
+    if ('primaryURL' in opts && opts.primaryURL) {
       builder.setPrimaryURL(opts.primaryURL);
     }
     if (opts.static) {
       addFilesRecursively(
         builder,
-        opts.static.baseURL || opts.baseURL,
+        opts.static.baseURL ?? opts.baseURL,
         opts.static.dir,
         opts
       );
@@ -84,11 +78,10 @@ export class WebBundlePlugin implements WebpackPluginInstance {
         ? (str: string) => compilation.getLogger(PLUGIN_NAME).info(str)
         : (str: string) => console.log(str);
 
-    const webBundle = maybeSignWebBundle(
-      builder.createBundle(),
-      opts,
-      (key: KeyObject) => infoLogger(`${new WebBundleId(key)}`)
-    );
+    let webBundle = builder.createBundle();
+    if ('integrityBlockSign' in opts) {
+      webBundle = getSignedWebBundle(webBundle, opts, infoLogger);
+    }
 
     if (isWebpackMajorV4()) {
       // @ts-expect-error Missing properties don't exist on webpack v4.
